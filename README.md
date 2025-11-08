72a208010625355203fc5fc4101c7c9b4746e73f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-11-08 17:17:06 +0100

    feat: GH action to configure a Yontrack build

---
+++ README.md
@@ -0,0 +1,66 @@
+# Ontrack GitHub actions: CLI Config
+
+GitHub action to install and configure the [Yontrack CLI](https://github.com/nemerosa/ontrack-cli), followed
+by the configuration of the build, branch & project in Yontrack, using
+a local configuration file.
+
+## Inputs
+
+### Yontrack CI configuration
+
+#### `config`
+
+Path to the local configuration file to use. Defaults to `.yontrack.ci.yml`.
+
+### Yontrack CLI configuration
+
+#### `version`
+
+Version of the [Yontrack CLI](https://github.com/nemerosa/ontrack-cli/releases) to install. If not specified, defaults to the latest available.
+
+#### `github-token`
+
+GitHub token to get the latest version of the CLI (when `version` is not provided).
+
+#### `url`
+
+URL of the Yontrack instance to target. If this input is set and the `token` one as well, this action will configure the CLI based on this information.
+
+#### `token`
+
+Authentication token to use to connect to Yontrack (required if URL is set). If this input is set and the `url` one as well, this action will setup the CLI based on this information.
+
+#### `cli-config`
+
+Optional name of the configuration to create for the CLI. Defaults to `default`.
+
+#### `conn-retry-count`
+
+Optional value to override max connection retry attempts. If not set, default ontrack-cli behavior applies. 
+
+#### `conn-retry-wait`
+
+Optional value to override max wait time between connection retry attempts. If not set, default ontrack-cli behavior applies. 
+
+## Outputs
+
+### `installed`
+
+Version which has actually been installed.
+
+## Example usage
+
+Setting the CLI automatically:
+
+```yaml
+- name: Setup the CLI
+  uses: nemerosa/ontrack-github-actions-cli-install
+  with:
+    github-token: ${{ github.token }}
+    url: <ontrack-url>
+    token: ${{ secrets.ONTRACK_TOKEN }}
+# This:
+# 1. installs the CLI
+# 2. configures the CLI
+# After this step, the `yontrack` command is available in the PATH.
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+const core = require('@actions/core');
+const client = require('@nemerosa/ontrack-github-actions-module-install');
+
+(async () => {
+    try {
+        await setup();
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
+
+async function setup() {
+
+    let url = core.getInput('url');
+    if (!url) {
+        url = process.env.YONTRACK_URL;
+    }
+    let token = core.getInput('token');
+    if (!token) {
+        token = process.env.YONTRACK_TOKEN;
+    }
+
+    // Installing and configuring the CLI
+    const {version, dir, cliExecutable} = await client.install({
+        version: core.getInput('version'),
+        githubToken: core.getInput('github-token'),
+        acceptDraft: false,
+        logging: true,
+        yontrackUrl: url,
+        yontrackToken: token,
+        yontrackUser: core.getInput('cli-config'),
+        connRetryCount: core.getInput('conn-retry-count'),
+        connRetryWait: core.getInput('conn-retry-wait'),
+    })
+
+    core.setOutput('installed', version);
+    core.addPath(dir)
+    core.info(`Yontrack CLI version ${version} installed`);
+
+    // Configuration
+
+    const path = core.getInput('config');
+    core.info(`Yontrack config at ${path}`);
+
+    // Building the command line arguments
+
+    const args = []
+    args.push('--file', path)
+    args.push('--ci', 'github')
+    args.push('--scm', 'github')
+    args.push('--output', 'env')
+
+    // List of environment variables to inject
+
+    const envNames = [
+        'GITHUB_SERVER_URL',
+        'GITHUB_REPOSITORY',
+        'GITHUB_REF_NAME',
+        'GITHUB_RUN_ID',
+        'GITHUB_RUN_NUMBER',
+        'GITHUB_WORKFLOW',
+        'GITHUB_EVENT_NAME',
+        'GITHUB_ACTIONS',
+        'GITHUB_SHA',
+    ]
+
+    for (const envName of envNames) {
+        args.push('--env', process.env[envName])
+    }
+
+    // Running the configuration
+
+    await exec.exec(cliExecutable, args)
+
+    // TODO Gets the outcome and injects it into the environment
+}
